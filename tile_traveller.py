c2149cf3d087fa927a29250b2831ac4e406c26e5
Bjartur Thorhalls <bjartur@Bjarturs-MacBook-Pro.local>
2019-09-17 09:19:48 +0000

    Forgot to save, algorithm is now included

---
+++ tile_traveller.py
@@ -0,0 +1,11 @@
+# Authors: Bjartur Þórhallsson, Guðjón Ingi Valdimarsson
+# Date: 17.09.2019
+
+'''
+1. Calculate which directions are available to the player.
+2. Print available directions.
+3. Get a direction from the player.
+4. Move the player accordingly, if the direction is unavailable ask for a new direction.
+6. Repeat until the player's position is at 3,1.
+7. Print a winning message and close the program.
+'''
